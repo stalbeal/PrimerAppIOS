63d3fc3b91d9901701ff9825954a0609f4878339
Stephany Berrio <Stephys@macbook-pro-de-stephys.local>
2015-11-16 19:37:22 -0500

    Initial Commit

---
+++ primerApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  primerApp
+//
+//  Created by Stephany Berrio on 16/11/15.
+//  Copyright © 2015 Stephany Berrio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
